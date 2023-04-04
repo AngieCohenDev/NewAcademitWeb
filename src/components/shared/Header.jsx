b501e308dd3cf2677ccfccf16346fbdc0ce7472e
Angie Cohen <angiedev2001@gmail.com>
2023-04-04 12:59:34 -0300

    Se cambiaron los colores de la pagina

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -9,9 +9,9 @@ const Header = () => {
   return (
     <header className="flex items-center justify-between xl:justify-start w-full py-4 px-8 h-[10vh] z-50">
       <div className="xl-1/6 text-center font-bold p-1">
-        <h1 className="text-3xl">ACADEMIT</h1>
+        <h1 className="text-3xl text-cyan-500">ACADEMIT</h1>
       </div>
-      <nav className={`fixed text-lg bg-white w-[80%] md:w-[40%] xl:w-full h-full ${showMenu ? "left-0" : "-left-full"} top-0 xl:static flex-1 flex flex-col xl:flex-row items-center justify-center xl:justify-end gap-10 transition-all duration-500`}>
+      <nav className={`fixed text-lg bg-white  text-cyan-500 w-[80%] md:w-[40%] xl:w-full h-full ${showMenu ? "left-0" : "-left-full"} top-0 xl:static flex-1 flex flex-col xl:flex-row items-center justify-center xl:justify-end gap-10 transition-all duration-500 z-50`}>
         <a href="#">Sobre Nosotros</a>
         <a href="#">Servicios</a>
         <a href="#">Cursos</a>
--- src/components/shared/Hero.jsx
+++ src/components/shared/Hero.jsx
@@ -4,13 +4,13 @@ import { BiMoviePlay } from "react-icons/bi";
 //Icons
 const Hero = () => {
   return (
-    <section className="h-[90vh] grid grid-cols-1 md:grid-cols-8">
-      <div className="md:col-span-5 flex items-center justify-center p-16 ">
+    <section className="min-h-[90vh] grid grid-cols-1 md:grid-cols-8 gap-8 md:gap-0">
+      <div className="md:col-span-5 flex items-center justify-center p-8 xl:p-12 ">
         <div className="flex flex-col gap-8">
-          <h1 className="text-7xl font-bold mb-1">Academit</h1>
+          <h1 className="text-7xl font-bold mb-1 text-cyan-500">Academit</h1>
           <p className="text-3xl font-medium text-gray-600">
             Programando la{" "}
-            <span className="text-3xl font-bold text-blue-300 py-1 px-4 border-4 border-blue-400 relative">
+            <span className="text-3xl font-bold text-blue-300 py-1 px-4 border-4 border-blue-400 relative inline-block">
               ÉLITE DEL MAÑANA,
               <RiCheckboxBlankCircleFill className="text-white text-xs absolute -left-3 -top-3 p-1 bg-blue-400 rounded-full box-content" />
               <RiCheckboxBlankCircleFill className="text-white text-xs absolute -right-3 -top-3 p-1 bg-blue-400 rounded-full box-content" />
@@ -19,20 +19,20 @@ const Hero = () => {
             </span>{" "}
             hoy
           </p>
-          <div className="flex items-center mt-8 gap-4">
-            <button className="bg-black text-white text-xl py-2 px-8 rounded-lg border-2 border-blue-400 ">
+          <div className="flex flex-col md:flex-row  items-center mt-8 gap-3">
+            <button className="md:w-full xl:w-[450px] bg-black text-white text-xl py-2 px-6 rounded-lg border-2 border-blue-400 ">
               Ver cursos
             </button>
-            <button className="flex items-center justify-start text-left gap-4 py-2 px-8 rounde text-xl ">
-              <BiMoviePlay className="bg-gray-200 text-blue-300 p-4 rounded-full box-content"/>
-              Ver nuestro video <br /> de presentación
+            <button className="w-full flex items-center justify-start text-left gap-3 py-2 px-8 rounde text-xl  ">
+              <BiMoviePlay className="bg-gray-200 text-blue-300 p-4 rounded-full box-content" />
+              Ver nuestro video <br /> de  presentación
             </button>
           </div>
         </div>
       </div>
       {/* Imagen */}
-      <div className="md:col-span-3 flex items-center justify-center">
-        <img src="logo.png" className="w-100 h-100 object-cover"  />
+      <div className="md:col-span-3 flex items-center justify-center relative p-6   ">
+        <img src="logo.png" className="w-100 h-100 object-cover" />
       </div>
     </section>
   );
